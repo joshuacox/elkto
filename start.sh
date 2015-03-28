9ac2a6dad13a5dedbd191ec54372e8bb35564cdf
Josh Cox <josh@webhosting.coop>
2015-03-28 11:06:25 -0500

    sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
-#!/bin/bash
+#!/bin/sh
 cp /opt/logstash/template.d/logstash.tpl /opt/logstash/conf.d/logstash.conf
 sed -i "s/REDIS_PORT_6379_TCP_ADDR/$REDIS_PORT_6379_TCP_ADDR/" /opt/logstash/conf.d/logstash.conf
 sed -i "s/REDIS_PORT_6379_TCP_PORT/$REDIS_PORT_6379_TCP_PORT/" /opt/logstash/conf.d/logstash.conf
 
-/app/bin/boot
+/bin/bash /app/bin/boot
