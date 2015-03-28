738d8a810616e45df784c677809dd6a4da4b494d
Josh Cox <josh@webhosting.coop>
2015-03-28 11:01:26 -0500

    start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-cp /etc/logstash/templates.d/logstash.tpl /etc/logstash/conf.d/logstash.conf
-sed -i "s/REDIS_PORT_6379_TCP_ADDR/$REDIS_PORT_6379_TCP_ADDR/" /etc/logstash/conf.d/logstash.conf
-sed -i "s/REDIS_PORT_6379_TCP_PORT/$REDIS_PORT_6379_TCP_PORT/" /etc/logstash/conf.d/logstash.conf
+cp /opt/logstash/templates.d/logstash.tpl /opt/logstash/conf.d/logstash.conf
+sed -i "s/REDIS_PORT_6379_TCP_ADDR/$REDIS_PORT_6379_TCP_ADDR/" /opt/logstash/conf.d/logstash.conf
+sed -i "s/REDIS_PORT_6379_TCP_PORT/$REDIS_PORT_6379_TCP_PORT/" /opt/logstash/conf.d/logstash.conf
 
 /app/bin/boot
